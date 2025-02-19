a93b3dcc4f9240dd3caccd618c8fbfcbd6749bc6
hieu.nguyen12 <hieu.nguyen12@onemount.com>
2025-02-19 20:07:30 +0700

    feat: tim kiem san pham

--- src/component/Product.jsx
+++ src/component/Product.jsx
@@ -1,10 +1,54 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Outlet, useSearchParams } from 'react-router-dom'
+
+const PRODUCTS = [{
+    id: "PRD01",
+    name: "coca cola",
+},
+{
+    id: "PRD02",
+    name: "pepsi",
+},
+{
+    id: "PRD03",
+    name: "7up",
+},
+{
+    id: "PRD04",
+    name: "Sting",
+},
+]
 
 const Product = () => {
+    const [searchPrams, setSearchParam] = useSearchParams();
+    const key = searchPrams.get("key");
+    const [inputValue, setInputvalue] = useState("");
+    const [searchValue, setSearchValue] = useState("");
+
+    const onClickSearch = () => {
+        setSearchParam({ key: inputValue });
+    }
+
+    const renderProducts = () => {
+        return PRODUCTS?.filter((prd) => !searchValue ? true : prd?.name?.includes(searchValue))
+            ?.map((prd) => {
+                return <div key={prd.id}>
+                    <h2>{`${prd.id} - ${prd.name}`}</h2>
+                </div>
+            })
+    }
+
+    useEffect(() => {
+        setSearchValue(key);
+    }, [key])
+
     return (
         <div>
             <h1>Product</h1>
+            <input onChange={(e) => setInputvalue(e.target.value)} placeholder='tim kiem' />
+            <button onClick={onClickSearch}>Search</button>
+
+            {renderProducts()}
             <Outlet />
         </div>
     )
